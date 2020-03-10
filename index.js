b76b14d2c1aa9950119111ffb251ba1e61f20401
Student_MA118 <Student_MA118@Student-MA118.local>
2020-03-10 09:49:18 -0400

    Some kinks

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 alert("Space Battle, a console log game!");
-let countShip = 0
+let countShip = 0;
 let humanShip ={
     hull : 20,
     firepower: 5,
@@ -23,6 +23,7 @@ class Space {
        if (humanShip.accuracy <= .7){
             alienShip[countShip].hull -= humanShip.firepower;
             console.log("Aliens have been hit!");
+            console.log("Your attack hits for " + humanShip.firepower);
         if (alienShip[countShip].hull <= 0){
             console.log("Alienship down!");
 
@@ -32,27 +33,32 @@ class Space {
     };
     
     function alienAttack(){
+        if (humanShip.hull <= 0){
+            console.log("You're dead!");
+            game === true;
+        }
         if (alienShip[countShip].accuracy >= .6 && alienShip[countShip].accuracy <= .8) {
             humanShip.hull -= alienShip.firepower;
             console.log("Human have been hit");
-         if (humanShip.hull <= 0){
-            console.log("You're dead!")
-        }
+            console.log("You've been hit for " + alienShip[countShip].firepower);
         }    
     };
 
     let game = false;
     
     function begin(){
-        while (countShip < 6 && humanShip.hull > 0 && game === false){
-        let action = prompt("Which action do you choose?");
+        while(countShip < 6 && humanShip.hull > 0 && game == false){
+        let action = prompt("Are you going to attack or retreat?");
             if (action.toLowerCase() === "attack"){
             humanAttack();
             gameWon();
+            if(game === true){
+                return;
+            }
             alienAttack();
             gameWon();
             } else if (action.toLowerCase() === 'retreat') {
-                console.log('Game Over You Lose!');
+                console.log('Game Over You Are A QUITTER!');
                 countShip = 6;
                 
             }
@@ -63,9 +69,10 @@ class Space {
             console.log('Aliens win!');
         }
            else if(countShip === 6) {
-            console.log('Game Over You Win!');
+            console.log('Game over you win!');
             game === true;
-        }
+        }   
+           
     };
 
         
